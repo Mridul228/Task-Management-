c0f60b6bd12789d1a2f9bafbce0f3dc9573540d3
Mridul <mridulhosen228@gmail.com>
2023-06-21 15:38:03 +0600

    updated page

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -7,8 +7,8 @@ import BackgroundImage from '../../assets/images/bg.png'
 export default function LandingPage() {
     return (
         <header style={HeaderStyle}>
-            <h1 className="main-title text-center">Home page</h1>
-            <p className="main-para text-center">join us now and don't waste time</p>
+            <h1 className="main-title text-center">Task Management System</h1>
+            <p className="main-para text-center">Please Login or Register</p>
             <div className="buttons text-center">
                 <Link to="/login">
                     <button className="primary-button">log in</button>
